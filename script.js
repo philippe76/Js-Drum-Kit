de5af1906b10d96c5b4969c87705cbb7563956cd
Philippe Lanougadere <soufiane92230@gmail.com>
2021-01-21 18:29:09 +0100

    ADD eventListener for click and keydown

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ let drumPad = document.querySelectorAll('.drum-pad');
 drumPad.forEach(item => {
     item.addEventListener('click', () => {
         let sound = document.getElementById(item.innerText);
-        sound.play()
+        sound.play();
     })
+})
 
+window.addEventListener('keydown', e => {
+    console.log(e.key);
+    let sound = document.getElementById(e.key.toUpperCase());
+    sound.play();
 })
