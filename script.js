ef24634342e4c1c935d61792c909981dacb37c59
Philippe Lanougadere <soufiane92230@gmail.com>
2021-01-23 14:06:11 +0100

    Display sound name when played

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 
 
 let drumPad = document.querySelectorAll('.drum-pad');
+let displaySound = document.querySelector('#display');
 
 drumPad.forEach(item => {
     item.addEventListener('click', () => {
         let sound = document.getElementById(item.innerText);
         sound.play();
+        let soundName = item.childNodes[1].innerText;
+        displaySound.innerHTML = `<p>${soundName}</p>`
     })
 })
 
 window.addEventListener('keydown', e => {
-    console.log(e.key);
     let sound = document.getElementById(e.key.toUpperCase());
     sound.play();
 })
