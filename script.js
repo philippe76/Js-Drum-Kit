8f5a58132291fd0c3824be86a1163c0ee6f8ff80
Philippe Lanougadere <soufiane92230@gmail.com>
2021-01-23 14:15:11 +0100

    Display sound name when played with keyboard

--- script.js
+++ script.js
@@ -15,4 +15,6 @@ drumPad.forEach(item => {
 window.addEventListener('keydown', e => {
     let sound = document.getElementById(e.key.toUpperCase());
     sound.play();
+    let soundName = sound.innerText;
+    displaySound.innerHTML = `<p>${soundName}</p>`
 })
